2b863e69f0acffc2941b54cf60df7e8df6672f2d
joao magnago <joaomagnago.wm@gmail.com>
2024-12-14 17:43:57 -0300

    chore: innitial commit and project setup

---
+++ src/app.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from './infra/database/prisma/prisma.service';
+
+@Module({
+  controllers: [],
+  providers: [PrismaService],
+})
+export class AppModule { }
